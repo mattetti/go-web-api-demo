42e62190fe32b00f5d4308630333579f7d02a47d
Matt Aimonetti <mattaimonetti@gmail.com>
2013-10-13 11:24:38 -0400

    added more information to the expvar data

---
+++ handlers/expvar.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+  "net/http"
+  "expvar"
+  "fmt"
+)
+
+func DebugVars(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	fmt.Fprintf(w, "{\n")
+	first := true
+	expvar.Do(func(kv expvar.KeyValue) {
+		if !first {
+			fmt.Fprintf(w, ",\n")
+		}
+		first = false
+		fmt.Fprintf(w, "%q: %s", kv.Key, kv.Value)
+	})
+	fmt.Fprintf(w, "\n}\n")
+}
--- main.go
+++ main.go
@@ -6,14 +6,22 @@ import (
 	"github.com/mattetti/go-web-api-demo/handlers"
 	"net/http"
 	"time"
+  "os"
+  "runtime"
 )
 
 var startedAt = expvar.NewString("startedAt")
+var serverPid = expvar.NewInt("pid")
 var serverBuild = expvar.NewString("build")
 
 // Note:
 // compile passing -ldflags "-X main.Build <build sha1>"
 var Build string
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	serverPid.Set(int64(os.Getpid()))
+}
 
 func main() {
 	startedAt.Set(time.Now().String())
@@ -21,5 +29,6 @@ func main() {
 
 	fmt.Println("About to start the server on port 8080")
 	http.HandleFunc("/hello/", handlers.HelloWorld)
+  http.HandleFunc("/debug", handlers.DebugVars)
 	http.ListenAndServe(":8080", nil)
 }
